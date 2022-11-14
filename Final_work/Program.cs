28b5e37e557263cba1ef9fcbca05fd0b126788d4
Belov Igor <grot.ik@mail.ru>
2022-11-14 19:00:34 +0200

    add_task

--- Final_work/Program.cs
+++ Final_work/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arr1 = { "Hello", "2", "world", ":-)" };
+string[] arr2 = { "1234", "1567", "-2", "computer science" };
+string[] arr3 = { "Russia", "Denmark", "Kazan" };
+
+int MAX_WORD_LENGTH = 3;
+
+string[] GetShortWordsArray(string[] array)
+{
+    int length = array.Length;
+    string[] result = new string[length];
+    int count = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i].Length <= MAX_WORD_LENGTH)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref result, count);
+
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    if (array.Length == 0)
+    {
+        Console.WriteLine("Пустой массив");
+    }
+    else
+    {
+        Array.ForEach(array, (str) => Console.Write($"{str} "));
+        Console.WriteLine();
+    }
+}
+
+void PrintTask(string[] array)
+{
+    Console.WriteLine("Исходный массив:");
+    PrintArray(array);
+    string[] shortArray = GetShortWordsArray(array);
+    Console.WriteLine($"Result array (words are less or equal than {MAX_WORD_LENGTH} symbols)");
+    PrintArray(shortArray);
+    Console.WriteLine();
+}
+
+PrintTask(arr1);
+PrintTask(arr2);
+PrintTask(arr3);
